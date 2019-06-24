76ca78ba87772e4a57104b6c48953b53d1334faa
wb9436 <1016065991@qq.com>
2019-06-24 10:38:04 +0800

    。。。

--- src/utils/secret.js
+++ src/utils/secret.js
@@ -3,8 +3,8 @@ import sha256 from 'js-sha256'
 import base64 from 'js-base64'
 import CryptoJS from 'crypto-js'
 
-const AES_KEY = CryptoJS.enc.Utf8.parse('1234567856789012')
-const AES_IV = CryptoJS.enc.Utf8.parse('1234567856789012')
+export const AES_KEY = '1234567856789012'
+export const AES_IV = '1234567856789012'
 
 export const Md5Encode = (val) => {
   return md5(val)
@@ -23,36 +23,18 @@ export const Base64Decode = (val) => {
 }
 
 export const AESEncode = (val, key, iv) => {
-  key = key ? CryptoJS.enc.Utf8.parse(key) : AES_KEY
-  iv = iv ? CryptoJS.enc.Utf8.parse(iv) : AES_IV
+  let keyHex = CryptoJS.enc.Base64.parse(key)
+  let ivHex = CryptoJS.enc.Base64.parse(iv)
 
-  key = CryptoJS.enc.Base64.stringify(key)
-  iv = CryptoJS.enc.Base64.stringify(iv)
-
-  console.log('AESEncode', key, iv)
-
-  let src = CryptoJS.enc.Utf8.parse(val)
-  src = CryptoJS.enc.Base64.stringify(src)
-
-  let encrypt = CryptoJS.AES.encrypt(src, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
-  return encrypt.toString()
+  let encrypted = CryptoJS.AES.encrypt(val, keyHex, { iv: ivHex, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
+  return encrypted.toString()
 }
 
 export const AESDecode = (val, key, iv) => {
-  key = key ? CryptoJS.enc.Utf8.parse(key) : AES_KEY
-  iv = iv ? CryptoJS.enc.Utf8.parse(iv) : AES_IV
+  let keyHex = CryptoJS.enc.Base64.parse(key)
+  let ivHex = CryptoJS.enc.Base64.parse(iv)
 
-  key = CryptoJS.enc.Base64.stringify(key)
-  iv = CryptoJS.enc.Base64.stringify(iv)
-
-  let decrypt = CryptoJS.AES.decrypt(val, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
-  return base64.Base64.decode(CryptoJS.enc.Utf8.stringify(decrypt))
+  let decrypt = CryptoJS.AES.decrypt(val, keyHex, { iv: ivHex, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
+  return Base64Decode(CryptoJS.enc.Base64.stringify(decrypt))
 }
 
-export const WeiXinDecode = (encryptedData, key, iv) => {
-  key = CryptoJS.enc.Base64.parse(key)
-  iv = CryptoJS.enc.Base64.parse(iv)
-
-  let decrypt = CryptoJS.AES.decrypt(encryptedData, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
-  return base64.Base64.decode(CryptoJS.enc.Base64.stringify(decrypt))
-}
--- src/view/secret/index.vue
+++ src/view/secret/index.vue
@@ -16,20 +16,21 @@
     },
     created () {
       const val = '依凡123'
-      console.log('Md5: ' + SecretUtils.Md5Encode(val))
-      console.log('SHA256: ' + SecretUtils.SHA256Encode(val))
+      // console.log('Md5: ' + SecretUtils.Md5Encode(val))
+      // console.log('SHA256: ' + SecretUtils.SHA256Encode(val))
 
-      let aesVal = SecretUtils.AESEncode(val)
-      console.log('AES Encode: ' + aesVal)
-      console.log('AES Decode: ' + SecretUtils.AESDecode(aesVal))
+      // let base64Val = SecretUtils.Base64Encode(val)
+      // console.log('Base64 Encode: ' + base64Val)
+      // console.log('Base64 Decode: ' + SecretUtils.Base64Decode(base64Val))
 
-      let base64Val = SecretUtils.Base64Encode(val)
-      console.log('Base64 Encode: ' + base64Val)
-      console.log('Base64 Decode: ' + SecretUtils.Base64Decode(base64Val))
+      // console.log('微信 AES Decode: ', SecretUtils.AESDecode('YhCsjoVWW86N6vPJdckI06auSLCiBgHA+pn5QIuekglnY0M0gJXLqAAG6ZRiff0LtDvG2wFu9KOFZqR0YclsdBGtGXDzCbiCjSjIM9W3Pw291K/tD3dN/RpY3qZ/3HkFgnfCsAB6AgPPQtbBBKRr7IZFeYFlLM3J1MxvJ1Y62CcBkPlwFXtcno8fcrbbkJSA3LQ4IgrOfUfI3I8hmzhMIoMXw0pZsmC0KviWsdyTOykpC9aTang/4HqPkougezbc9dSUe6C9SvE8DHcNtaTmzlUIdNYQ/AuCJLxOfkRuWE2vvtrdjt/2PtX03ENIthNLMYj8lZ+PT8mx1shxWvt5C/+TqjfVKlcV7M3Bkw8ORb/HM7KgE89Kb448dNRcYE+aYv/zQ4eBWaxQpkggKLJd6A/Dxmk9z1dpMx4vbYWxobEtlecI8SlbbgJmOxmxUmrBx5LUIbYf45ZPSRRgxnxbri+/ofuRL+Yxuw2kpPMxCFtIUnDjcSIlxGF7SBywKPWQfJjbxcmf3g9AhPTNOMHY18mSA10ZTWXcGTJt8CRIZVo=', 'QIpx19G0eoFYYfw3+Ssz5A==', 'bBY4NNZJfVqTn80PmFko/Q=='))
 
-      console.log('微信 AES Decode: ', SecretUtils.WeiXinDecode('YhCsjoVWW86N6vPJdckI06auSLCiBgHA+pn5QIuekglnY0M0gJXLqAAG6ZRiff0LtDvG2wFu9KOFZqR0YclsdBGtGXDzCbiCjSjIM9W3Pw291K/tD3dN/RpY3qZ/3HkFgnfCsAB6AgPPQtbBBKRr7IZFeYFlLM3J1MxvJ1Y62CcBkPlwFXtcno8fcrbbkJSA3LQ4IgrOfUfI3I8hmzhMIoMXw0pZsmC0KviWsdyTOykpC9aTang/4HqPkougezbc9dSUe6C9SvE8DHcNtaTmzlUIdNYQ/AuCJLxOfkRuWE2vvtrdjt/2PtX03ENIthNLMYj8lZ+PT8mx1shxWvt5C/+TqjfVKlcV7M3Bkw8ORb/HM7KgE89Kb448dNRcYE+aYv/zQ4eBWaxQpkggKLJd6A/Dxmk9z1dpMx4vbYWxobEtlecI8SlbbgJmOxmxUmrBx5LUIbYf45ZPSRRgxnxbri+/ofuRL+Yxuw2kpPMxCFtIUnDjcSIlxGF7SBywKPWQfJjbxcmf3g9AhPTNOMHY18mSA10ZTWXcGTJt8CRIZVo=', 'QIpx19G0eoFYYfw3+Ssz5A==', 'bBY4NNZJfVqTn80PmFko/Q=='))
-      // console.log('微信 AES Decode: ', SecretUtils.WeiXinDecode('U2FsdGVkX186lvaDUuO0flY50+mJJORlr5EC/ieN0ig=', 'MTIzNDU2Nzg1Njc4OTAxMg==', 'MTIzNDU2Nzg1Njc4OTAxMg=='))
+      // let userInfo = { 'openId': 'o3DRs5SafuprF9Nqsq8ls8cd4z9w', 'nickName': '依凡', 'gender': 1, 'language': 'zh_CN', 'city': 'Pudong New District', 'province': 'Shanghai', 'country': 'China', 'avatarUrl': 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eroIXY4hLVAsACuQj7pClLIKjoLx5HJ8tVgDeusLqBMV3bFp14PzejTgw0TgGbt156Wqp4J2Wzyibw/132', 'unionId': 'oOTnV0jqeLXW2NJ3LWwRlFRRCNN0', 'watermark': { 'timestamp': 1560328058, 'appid': 'wxef810c89d728cc72' } }
+      // console.log('微信 AES Encode: ', SecretUtils.AESEncode(JSON.stringify(userInfo), SecretUtils.AES_KEY, SecretUtils.AES_IV))
+      // console.log('微信 AES Decode: ', SecretUtils.AESDecode('hM3LKlo8h5UE322fCVbyaG6tdX6nK9FBVLhTC56muOxfQythUuPkAoYCLixPsTIud4Vu5wsqnkchd9Sdue0q8FV9qcIyVZEpbhZktzbfCAbgRF9c2KRk4j5l8R0FqGRNsbXXo2AI7DdFO0CH4ik8qvurfLk4mMayyD6YTCX5zMwqozDnSlbI4ND1Hw5FdlzYwSdK1moEEOQ76tjvfC5IrSdD0RgCa8rg08AeN0LCwQl/3RUiBH+eQyUNKcF6gedJtIBRc8J3OIrnicvJHeY9BvaLJ27dx6uwwFwJzjb1N8L68a0KFRQvgW9Ud4ITHT0E4o4bEHrBcqx8MBHkVrPeitxTjGp93LlDdn0BzH5uMwEmRc5tBPXK1GmrWzaSJ+SN18D3txJ5GS36y0YoDjOs1R6dSpyTVuvKqiP086+uLsq3L+BMkRp5RNDcKW6lccqZIyfWRlH95720smWV1ijZQyDwS1qmkykZg+QdSH8UNIDGmO3Swu/DNzmZyUUrg0rLxRAOyDWK6nu/idAkcmjDGsWeFox/aI7TeDbWeEWi01w=', SecretUtils.AES_KEY, SecretUtils.AES_IV))
 
+      console.log('微信 AES Encode: ', SecretUtils.AESEncode(val, SecretUtils.AES_KEY, SecretUtils.AES_IV))
+      console.log('微信 AES Decode: ', SecretUtils.AESDecode('pXx+2p26pT1a6+l5W0+ksQ==', SecretUtils.AES_KEY, SecretUtils.AES_IV))
     }
   }
 </script>
